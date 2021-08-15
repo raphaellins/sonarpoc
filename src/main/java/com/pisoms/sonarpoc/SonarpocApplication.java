8f70aa80386a179721b1f1cdc57bf21a7b3a985f
Raphael lins <raphalinns@gmail.com>
2021-08-14 22:06:49 -0300

    initial commit

---
+++ src/main/java/com/pisoms/sonarpoc/SonarpocApplication.java
@@ -0,0 +1,13 @@
+package com.pisoms.sonarpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pisoms/sonarpoc/SonarpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pisoms.sonarpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
